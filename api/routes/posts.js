29da2a1ddb547e063bb5d5a37c0f1bdd8acdd749
siampathan005@gmail.com <siampathan005@gmail.com>
2022-10-20 22:47:04 +0600

    complete the rest api just want to test it

--- api/routes/posts.js
+++ api/routes/posts.js
@@ -56,6 +56,28 @@ router.put("/:id/like", async (req, res) => {
   }
 });
 //get a post
-//get time posts
+router.get("/:id", async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+    res.status(200).json(post);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+//get timeline posts
+router.get("/timeline/all", async (req, res) => {
+  try {
+    const currentUser = await User.findById(req.body.userId);
+    const userPostes = await Post.find({ userId: currentUser._id });
+    const friendPosts = await Promise.all(
+      currentUser.followings.map((friendId) => {
+        return Post.find({ userId: friendId });
+      })
+    );
+    res.json(userPostes.concat(...friendPosts));
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 
 module.exports = router;
