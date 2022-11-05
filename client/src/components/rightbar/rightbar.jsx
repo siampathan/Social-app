a981beb7bc8a1cf0917f2a6fa3ddf8fce336a450
siampathan005@gmail.com <siampathan005@gmail.com>
2022-11-05 19:54:08 +0600

    complete basic level design

--- client/src/components/rightbar/rightbar.jsx
+++ client/src/components/rightbar/rightbar.jsx
@@ -3,7 +3,90 @@ import "./rightbar-style.css";
 const RightBar = () => {
   return (
     <div className="rightbar">
-      <h2>Right bar</h2>
+      <div className="rightbarWrapper">
+        <div className="birthdayContainer">
+          <img className="birthdayImg" src="assets/gift.png" alt="" />
+          <span className="birthdayText">
+            <b> Pola Foster </b> and <b> 3 other firends </b> have birthday
+            today
+          </span>
+        </div>
+        <img className="rightbarAd" src="assets/ad.png" alt="" />
+        <h4 className="rightbarTitle">Online Friends</h4>
+        <ul className="rightbarFriendList">
+          <li className="rightbarFriend">
+            <div className="rightbarProfileImgContainer">
+              <img
+                className="rightbarProfileImg"
+                src="assets/person/3.jpeg"
+                alt=""
+              />
+              <span className="rightbarOnline"></span>
+            </div>
+            <span className="rightbarUserName">Jhone Carft</span>
+          </li>
+
+          <li className="rightbarFriend">
+            <div className="rightbarProfileImgContainer">
+              <img
+                className="rightbarProfileImg"
+                src="assets/person/taspi.jpg"
+                alt=""
+              />
+              <span className="rightbarOnline"></span>
+            </div>
+            <span className="rightbarUserName">Tabassum Taspiya</span>
+          </li>
+
+          <li className="rightbarFriend">
+            <div className="rightbarProfileImgContainer">
+              <img
+                className="rightbarProfileImg"
+                src="assets/person/taspi2.png"
+                alt=""
+              />
+              <span className="rightbarOnline"></span>
+            </div>
+            <span className="rightbarUserName">Taspiya Taspi</span>
+          </li>
+
+          <li className="rightbarFriend">
+            <div className="rightbarProfileImgContainer">
+              <img
+                className="rightbarProfileImg"
+                src="assets/person/3.jpeg"
+                alt=""
+              />
+              <span className="rightbarOnline"></span>
+            </div>
+            <span className="rightbarUserName">Jhone Carft</span>
+          </li>
+
+          <li className="rightbarFriend">
+            <div className="rightbarProfileImgContainer">
+              <img
+                className="rightbarProfileImg"
+                src="assets/person/3.jpeg"
+                alt=""
+              />
+              <span className="rightbarOnline"></span>
+            </div>
+            <span className="rightbarUserName">Jhone Carft</span>
+          </li>
+
+          <li className="rightbarFriend">
+            <div className="rightbarProfileImgContainer">
+              <img
+                className="rightbarProfileImg"
+                src="assets/person/3.jpeg"
+                alt=""
+              />
+              <span className="rightbarOnline"></span>
+            </div>
+            <span className="rightbarUserName">Jhone Carft</span>
+          </li>
+        </ul>
+      </div>
     </div>
   );
 };
