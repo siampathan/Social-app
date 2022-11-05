c04f833ef2a6ada2a3e1e8884ca398e640853c2f
siampathan005@gmail.com <siampathan005@gmail.com>
2022-11-05 21:17:39 +0600

    finish home page

--- client/src/components/post/post.jsx
+++ client/src/components/post/post.jsx
@@ -1,8 +1,17 @@
 import MoreVertIcon from "@mui/icons-material/MoreVert";
+import { useState } from "react";
 import { Users } from "../../dummyData";
 import "./post-style.css";
 
 const Post = ({ post }) => {
+  const [like, setLike] = useState(post.like);
+  const [isLiked, setIsLiked] = useState(false);
+
+  const likeHandler = () => {
+    setLike(isLiked ? like - 1 : like + 1);
+    setIsLiked(!isLiked);
+  };
+
   return (
     <div className="post">
       <div className="postWrapper">
@@ -28,9 +37,19 @@ const Post = ({ post }) => {
         </div>
         <div className="postBottom">
           <div className="postBottomLeft">
-            <img className="likeIcon" src="/assets/like.png" alt="" />
-            <img className="likeIcon" src="/assets/heart.png" alt="" />
-            <span className="likeCounter">{post.like} people like it</span>
+            <img
+              className="likeIcon"
+              src="/assets/like.png"
+              onClick={likeHandler}
+              alt=""
+            />
+            <img
+              className="likeIcon"
+              src="/assets/heart.png"
+              onClick={likeHandler}
+              alt=""
+            />
+            <span className="likeCounter">{like} people like it</span>
           </div>
           <div className="postBottomRight">
             <span className="postCommentText"> {post.comment} comments</span>
